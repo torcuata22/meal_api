7c39078821f59cf545dbb85621193c51f9bd4d45
torcuata22 <marilynjmarquez@gmail.com>
2023-04-27 10:43:37 -0500

    database populated

--- meal_project/meal_app/models.py
+++ meal_project/meal_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Meal(models.Model):
     name = models.CharField(max_length=50, blank = True, null = True)
     category = models.CharField(max_length=10, blank = True, null = True)
-    intructions = models.TextField(max_length=5000, blank = True, null = True)
+    instructions = models.TextField(max_length=5000, blank = True, null = True)
     yt_link = models.URLField(blank=True, null=True)
     slug = models.SlugField(default="test")
     image_url = models.URLField(blank=True, null=True)
--- meal_project/meal_app/views.py
+++ meal_project/meal_app/views.py
@@ -1,9 +1,45 @@
 from django.shortcuts import render
+import requests
+from .models import Meal
 
 # Create your views here.
 
 def get_meals(request):
-    return render(request, 'meal_app/meal.html')
+    #create empty dictionary to hold all meals:
+    all_meals = {}
+    #check if the get request has a name in it:
+    if 'name' in request.GET:
+        name = request.GET['name']
+        #from the API docs, get the url for the query:
+        url ='https://www.themealdb.com/api/json/v1/1/search.php?s=%s' % name
+        #create response object and convert to JSON:
+        response = requests.get(url) #This is REQUESTS (plural, the library), NOT singular
+        data = response.json()
+        print(data)
+        #Save the list generated in JSON file
+        meals = data['meals']
+        print(meals)
+
+        #loop through json, equate model fields with json object values, and populate the database:
+        for meal in meals:
+            meal_data = Meal(
+                name = meal['strMeal'],
+                category = meal['strCategory'],
+                instructions = meal['strInstructions'],
+                yt_link = meal['strYoutube'],
+                slug = meal['idMeal'],
+                image_url = meal['strMealThumb'],
+            )
+            #save the data:
+            meal_data.save()
+            #retrieve all the meals and order by their id number from the database:
+            all_meals = Meal.objects.all().order_by('id')
+
+    return render(request, 'meal_app/meal.html', {'all_meals':all_meals})
 
 def meal_detail(request):
     pass
+
+
+
+#pip install requests (to build url that will connect to api)
